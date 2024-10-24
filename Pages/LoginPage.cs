f3245eef756bcf9d647e3483fe5eaac631db0081
yogesh <yogeshh.aggarwal@gmail.com>
2024-10-24 15:36:08 +0530

    New Folder for POM test cases

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playwritetest_001.Pages
+{
+    public class LoginPage
+    {
+        private IPage _page;
+        private readonly ILocator _lnkLogin;
+        private readonly ILocator _txtUserName;   
+        private readonly ILocator _txtPassword;
+        private readonly ILocator _btnLogin;
+        private readonly ILocator _lnkEmployeeDetails;   
+        
+        public LoginPage(IPage page) {
+            _page = page;
+            _lnkLogin = _page.Locator("txt=Login");
+            _txtUserName = _page.Locator("#UserName");
+            _txtPassword = _page.Locator("#Password");
+            _btnLogin = _page.Locator("txt=Log in");
+            _lnkEmployeeDetails= _page.Locator("text = Employee Details");
+        }
+
+        public async Task ClickLogin() => await _lnkLogin.ClickAsync();
+
+
+        public async Task Login(String userName, String password)
+        {
+
+            await  _txtUserName.FillAsync(userName);
+            await  _txtUserName.FillAsync(password);
+            await  _btnLogin.ClickAsync();
+
+
+        }
+    }
+}
---
+++ Pages/LoginPageUpgraded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playwritetest_001.Pages
+{
+    public class LoginPageUpgraded
+    {
+        private IPage _page;
+
+        public LoginPageUpgraded(IPage page) => _page = page; 
+        private  ILocator _lnkLogin  => _page.Locator("txt=Login");
+        private  ILocator _txtUserName => _page.Locator("#UserName");   
+        private  ILocator _txtPassword => _page.Locator("#Password");
+        private  ILocator _btnLogin => _page.Locator("txt=Log in");
+        private  ILocator _lnkEmployeeDetails => _page.Locator("text = Employee Details");   
+        
+         
+        public async Task ClickLogin() => await _lnkLogin.ClickAsync();
+
+
+        public async Task Login(String userName, String password)
+        {
+
+            await  _txtUserName.FillAsync(userName);
+            await  _txtUserName.FillAsync(password);
+            await  _btnLogin.ClickAsync();
+
+
+        }
+
+        public async Task <bool> IsEmployeeDetalsExist() => await _lnkEmployeeDetails.IsVisibleAsync();
+    }
+}
